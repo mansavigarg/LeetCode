a64ff79b47bf8a60ddde822263b300e6ce5ebda7
Mansavi <132539340+mansavigarg@users.noreply.github.com>
2024-10-20 16:49:04 +0530

    Time: 0 ms (100%), Space: 9 MB (10.47%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // custom function to revserse an Vector
+    void reverseVector(vector<int>& arr){
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+
+        while(start<end){
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+
+
+
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         // Step 1: Transpose 2D matrix
@@ -15,6 +30,9 @@ public:
             // during this reverse, har row k elements reverse ho rhe hai 
             // phele 0 row k element reverse hue then 1 then all 
             reverse( matrix[i].begin(), matrix[i].end() );
+
+            // This is custom reverse vector ka funcation
+                // reverseVector(matrix[i]);
         }
     }
 };
